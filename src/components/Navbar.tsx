bcc3966b0f2ae4fbe3fe16621d43e1eb64dab626
mohamed <ma0068664@gmail.com>
2023-11-15 19:48:12 +0200

    feat(navbar): add navbar logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Icons } from './Icons'
+
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
     <div className='fixed top-0 inset-x-0 h-fit bg-zinc-300 border-b border-zinc-500'>
-        <div className='container max-w-7xl '>
+        <div className='container max-w-7xl flex justify-between py-2'>
+          <Link href='/'>
+            <Icons.logo className='w-12 h-12'/>
+          </Link>
           Navbar
         </div>
     </div>
